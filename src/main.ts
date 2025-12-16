22caec418a4c2cb667fbe56f84402585ba66ca51
GianniC88 <gianni.colucci.88@gmail.com>
2025-12-16 14:09:35 +0100

    Aggiungi la funzione isActress per la validazione dei dati e implementa getActress per recuperare le attrici dal server

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,20 @@ type ActressNationality =
   | "South Korean"
   | "Chinese";
 
+const validNationalities: ActressNationality[] = [
+  "American",
+  "British",
+  "Australian",
+  "Israeli-American",
+  "South African",
+  "French",
+  "Indian",
+  "Israeli",
+  "Spanish",
+  "South Korean",
+  "Chinese",
+];
+
 type Actress = Person & {
   most_famous_movies: [string, string, string];
   awards: string;
@@ -52,6 +66,53 @@ type Actress = Person & {
 // La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
 
 // Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === "object" &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every((m) => typeof m === "string") &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string" &&
+    validNationalities.includes(dati.nationality as ActressNationality)
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/users/${id}`);
+    const dati: unknown = await response.json();
+    if (!isActress(dati)) {
+      throw new Error("formato dati sconosciuto");
+    }
+    return dati;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante il recupero dei dati:", error);
+    } else {
+      console.error("errore sconosciuto:", error);
+    }
+    return null;
+  }
+}
 
 // 📌 Milestone 4
 // Crea una funzione getAllActresses che chiama:
